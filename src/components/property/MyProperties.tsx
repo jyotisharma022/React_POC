d4385c9817701c88586501c40e41522c94b1434a
jyotisharma022 <jyoti.sharma280689@gmail.com>
2021-04-01 10:44:05 +0530

    react project with json data access

---
+++ src/components/property/MyProperties.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {PropertyOverview} from './PropertyOverview'
+
+export const MyProperties = function () {
+    return (
+        <div>
+            <PropertyOverview></PropertyOverview>
+        </div>
+    )
+}
+
+
+
+export default MyProperties
